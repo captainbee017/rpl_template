c40b24f72674ad6e2dc247109008f12bde1c72f5
Abhishek J. Karki <abhishek.karki@merojob.com>
2017-04-28 17:00:53 +0545

    changes

--- councelar/settings.py
+++ councelar/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'v#n6p7actx!%3sc%3$8%35gr2fqv7#ww_41mjwycg55-zwvjm('
 DROPBOX_KEY = 'gqV5tUUVyqYAAAAAAAAAmNVH370mqmMzDTE12v6lOn87qssOqqPVjSQYT5Tm4_FB'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['djungbahadur.pythonanywhere.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = []
 
 
 LOGIN_URL = 'cp_auth'
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'councelar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'rpldb',
+        'NAME': 'counselor',
         # 'ENGINE': 'django.db.backends.mysql',
         # 'NAME': 'counselor',
         # 'USER': 'root',
@@ -157,10 +157,10 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
-# GRAPH_MODELS = {
-#   'all_applications': True,
-#   'group_models': True,
-# }
+GRAPH_MODELS = {
+  'all_applications': True,
+  'group_models': True,
+}
 
 REDACTOR_OPTIONS = {'lang': 'en'}
 REDACTOR_UPLOAD = 'uploads/'
